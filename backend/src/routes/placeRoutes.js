9f481192294eccb0750aa166148647af42a39a69
Armin Youuu <57670723+arminyouuu@users.noreply.github.com>
2025-10-13 00:08:26 +0330

    Updated placeRoutes.js

--- backend/src/routes/placeRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import PlaceController from '../controllers/PlaceController.js';
-
-const router = express.Router();
-
-router.get('/', PlaceController.getAll);
-router.get('/:id', PlaceController.getById);
-
-export default router;
