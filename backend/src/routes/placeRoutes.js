a93489a491ba7345e0b8310876e3bffbf2ecea1d
Armin Youuu <57670723+arminyouuu@users.noreply.github.com>
2025-10-13 00:29:43 +0330

    Updated placeRoutes.js

--- backend/src/routes/placeRoutes.js
+++ backend/src/routes/placeRoutes.js
@@ -5,5 +5,8 @@ const router = express.Router();
 
 router.get('/', PlaceController.getAll);
 router.get('/:id', PlaceController.getById);
+router.post('/', PlaceController.create);
+router.put('/:id', PlaceController.update);
+router.delete('/:id', PlaceController.delete);
 
 export default router;
