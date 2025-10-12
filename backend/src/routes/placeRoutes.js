ed929216b03f2341585da9df4908055d9c584c01
Armin Youuu <57670723+arminyouuu@users.noreply.github.com>
2025-10-13 00:09:43 +0330

    Updated categoryRoutes.js

--- backend/src/routes/placeRoutes.js
+++ backend/src/routes/placeRoutes.js
@@ -1,13 +1,9 @@
 import express from 'express';
 import PlaceController from '../controllers/PlaceController.js';
-import { authenticateApiKey, requireRead, requireWrite } from '../middleware/apiKeyAuth.js';
 
 const router = express.Router();
 
-router.get('/', authenticateApiKey, requireRead, PlaceController.getAll);
-router.get('/:id', authenticateApiKey, requireRead, PlaceController.getById);
-router.post('/', authenticateApiKey, requireWrite, PlaceController.create);
-router.put('/:id', authenticateApiKey, requireWrite, PlaceController.update);
-router.delete('/:id', authenticateApiKey, requireWrite, PlaceController.delete);
+router.get('/', PlaceController.getAll);
+router.get('/:id', PlaceController.getById);
 
 export default router;
