c85c50b144a7a3063d95cdf22d7d0716480908e6
Armin Youuu <57670723+arminyouuu@users.noreply.github.com>
2025-10-13 00:29:44 +0330

    Updated categoryRoutes.js

--- backend/src/routes/categoryRoutes.js
+++ backend/src/routes/categoryRoutes.js
@@ -5,5 +5,8 @@ const router = express.Router();
 
 router.get('/', CategoryController.getAll);
 router.get('/:id', CategoryController.getById);
+router.post('/', CategoryController.create);
+router.put('/:id', CategoryController.update);
+router.delete('/:id', CategoryController.delete);
 
 export default router;
